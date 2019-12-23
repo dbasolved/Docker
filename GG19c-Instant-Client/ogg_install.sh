a1e373d737a12102a2d9f254a3d6727b5db7ac01
Bobby Curtis <bobby.curtis@oracle.com>
2019-12-23 14:48:28 -0500

    minor changes

--- GG19c-Instant-Client/ogg_install.sh
+++ GG19c-Instant-Client/ogg_install.sh
@@ -21,21 +21,6 @@ rm -f ${SOFTWARE_HOME}/ora-response/oggcore.rsp
 
 python ${SOFTWARE_HOME}/python/ggSelfSignCerts.py
 
-#echo
-cp ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp.tmpl ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp 
-sed -i -e "s|###DEPLOYMENT_NAME###|Atlanta|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###PWD###|WElcome12345##|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###CREATESM###|true|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###SMPORT###|16000|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###ASPORT###|16001|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###DSPORT###|16002|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###RSPORT###|16003|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###PMPORT###|16004|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###PMUDPPORT###|16005|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-echo
-${OGG_HOME}/bin/oggca.sh -silent -responseFile ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-rm -f ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-
 echo
 echo 'INSTALLER: Done'
 echo
--- GG19c-Instant-Client/scripts/addDeployment.sh
+++ GG19c-Instant-Client/scripts/addDeployment.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 cp ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp.tmpl ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp 
-sed -i -e "s|###DEPLOYMENT_NAME###|Boston|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
+sed -i -e "s|###DEPLOYMENT_NAME###|Atlanta|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
 sed -i -e "s|###PWD###|WElcome12345##|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|HOST_SERVICEMANAGER=gg19c|HOST_SERVICEMANAGER=localhost|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###CREATESM###|false|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
+sed -i -e "s|###CREATESM###|true|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
 sed -i -e "s|###SMPORT###|16000|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###ASPORT###|17001|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###DSPORT###|17002|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###RSPORT###|17003|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###PMPORT###|17004|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
-sed -i -e "s|###PMUDPPORT###|17005|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
+sed -i -e "s|###ASPORT###|16001|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
+sed -i -e "s|###DSPORT###|16002|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
+sed -i -e "s|###RSPORT###|16003|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
+sed -i -e "s|###PMPORT###|16004|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
+sed -i -e "s|###PMUDPPORT###|16005|g" ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
 echo
 $OGG_HOME/bin/oggca.sh -silent -responseFile ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
 rm -f ${SOFTWARE_HOME}/ora-response/oggca_deployment.rsp
